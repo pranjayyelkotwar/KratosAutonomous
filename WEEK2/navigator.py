dbca5e70a4225712a1506b06f5d1d1483aacb2da
Pranjay Yelkotwar <66084036+pranjayyelkotwar@users.noreply.github.com>
2023-08-29 00:01:04 +0530

    Update navigator.py

--- WEEK2/navigator.py
+++ WEEK2/navigator.py
@@ -1,1 +1,27 @@
+#! /usr/bin/env python3
+ 
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+ 
+disToObstacle = 0.55
+ 
+def callback(msg):
+	print(f"Distance is {msg.ranges[0]}")
+	if msg.ranges[0] <= disToObstacle :
+		move.angular.z = 1
+		move.linear.x = 0
+		pub.publish(move)
+		#speed changed
+		rospy.sleep(0.4)
+	move.linear.x = 0.5
+	move.angular.z = 0
+	pub.publish(move)
 
+rospy.init_node('Week2')
+sub = rospy.Subscriber('/scan', LaserScan, callback)
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+rate = rospy.Rate(2)
+move = Twist()
+
+rospy.spin()
